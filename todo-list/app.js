e91711c57f41ee5dc44acf8e440d5c13b7bcf06b
s0l0d0l0011 <jframonex@gmail.com>
2020-12-22 10:10:57 -0500

    To-Do-List App project In Vanilla JS

---
+++ todo-list/app.js
@@ -0,0 +1,58 @@
+// selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+
+// functions 
+
+function addTodo(event) {
+    //prevent form from submitting
+    event.preventDefault();
+
+    // To do DIV:
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    //Create LI
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+
+    //Checked marked button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"> </i>'
+    completedButton.classList.add('completed-btn');
+    todoDiv.appendChild(completedButton);
+
+    //Trash button 
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"> </i>'
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+
+    //Append to list
+
+    todoList.appendChild(todoDiv);
+
+
+    // clear to do input value:
+    todoInput.value = "";
+}
+
+
+//Creates the delete check 
+function deleteCheck(e) {
+    console.log(e.target);
+}
