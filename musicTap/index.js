01c0fc752092c60d5687b6704bde6e74e245d09e
s0l0d0l0011 <jframonex@gmail.com>
2020-12-26 11:14:57 -0500

    Music Tap WebAPP using vanilla JS

---
+++ musicTap/index.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+
+    // Lets get going with the sound 
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function () {
+            sounds[index].play();
+
+        });
+    });
+});
+
